d9b1677e09fb378a550d098de1cff4962ccd6221
smirgol <14124899+smirgol@users.noreply.github.com>
2024-02-12 16:33:15 +0100

    add workaround for asyncio python bug on win32

--- resources/lib/view.py
+++ resources/lib/view.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 import asyncio
-import re
 
 from resources.lib.api import API
 from resources.lib.model import ListableItem, EpisodeData, Args, SeasonData, SeriesData, PlayableItem
